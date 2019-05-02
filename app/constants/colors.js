80ac699c2767ee98b2e2f4ba8a1dc9b100b5e1a0
cglotr <amir.ariffin.920404@gmail.com>
2019-05-03 00:34:18 +0800

    change bottom bar look

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -1,4 +1,5 @@
 export default {
+  GREY: '#808080',
   MEDIUM_SEA_GREEN: '#3CB371',
   TOMATO: '#FF6347',
   WHITE_SMOKE: '#F5F5F5'
--- app/navigation/navigators/index.js
+++ app/navigation/navigators/index.js
@@ -1,13 +1,50 @@
+import React from 'react'
+import { Icon } from 'react-native-elements'
 import { createBottomTabNavigator } from 'react-navigation'
+import { colors } from 'dl/constants'
 import { Deleted } from 'dl/screens'
 import MainStack from './main-stack'
 
 export default createBottomTabNavigator(
   {
-    Deleted,
-    MainStack
+    Deleted: {
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => {
+          return (
+            <Icon
+              color={tintColor}
+              name='delete'
+              type='material'
+            />
+          )
+        }
+      },
+      screen: Deleted
+    },
+    MainStack: {
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => {
+          return (
+            <Icon
+              color={tintColor}
+              name='spa'
+              type='material'
+            />
+          )
+        }
+      },
+      screen: MainStack
+    }
   },
   {
-    initialRouteName: 'MainStack'
+    initialRouteName: 'MainStack',
+    tabBarOptions: {
+      activeTintColor: colors.MEDIUM_SEA_GREEN,
+      inactiveTintColor: colors.GREY,
+      showLabel: false,
+      style: {
+        borderTopWidth: 0
+      }
+    }
   }
 )
